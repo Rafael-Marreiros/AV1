91ddb589a6888b30bcd6a7fe369e458cc543b9ae
Rafael <rafaelmarreiros007@gmail.com>
2022-11-29 13:27:15 -0300

    finalização da AV1

---
+++ AV1/AV1/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using AV1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AV1.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            HomeModel home = new HomeModel();
+
+            home.Nome = "Daniel";
+            home.Professor = "Tecnicas de Programação";
+
+            return View(home);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AV1/AV1/Models/HomeModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AV1.Models
+{
+    public class HomeModel
+    {
+        public string Nome { get; set; }
+        public string Professor { get; set; }
+    }
+}
